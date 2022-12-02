f12fd347c3bf894d8f472d0d1afd703260d91b68
SEM <SEM@DESKTOP-4GKM6HK>
2022-12-02 11:36:08 +0300

    Proje dosyası ekle.

---
+++ Scope3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Başla();
+
+void Başla()
+{
+    double x = 100;
+    Console.WriteLine("x ilk değeri" + x);
+    x = Kokal(x);
+    Console.WriteLine("x metoddan döndükten sonrası değeri" + x);
+   
+  
+
+}
+
+double Kokal(double x)
+{
+    return Math.Sqrt(x);
+    
+}
